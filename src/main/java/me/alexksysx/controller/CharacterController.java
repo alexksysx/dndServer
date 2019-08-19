7cb227ed2ea5d4cd5b112b90235ce757e4fe197d
alexksysx <leha.bar.ru@gmail.com>
2019-08-19 12:42:50 +0300

    Character Rest controller

--- src/main/java/me/alexksysx/controller/CharacterController.java
+++ src/main/java/me/alexksysx/controller/CharacterController.java
@@ -1,49 +1,36 @@
 package me.alexksysx.controller;
 
-
-import me.alexksysx.exceptions.NotFoundException;
 import me.alexksysx.model.Character;
+import me.alexksysx.model.Characters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/character")
+@RequestMapping("/characters")
 public class CharacterController {
-    private final AtomicLong counter = new AtomicLong();
-    private List<Character> characters = new ArrayList<>();
 
-    @RequestMapping("/create")
-    public Character create (@RequestParam(value = "name", defaultValue = "CharacterName") String name,
-                         @RequestParam(value = "race", defaultValue = "Human") String race,
-                         @RequestParam(value = "subrace", defaultValue = "Common") String subrace,
-                         @RequestParam(value = "gameClass", defaultValue = "Barbarian") String gameClass,
-                         @RequestParam(value = "strength", defaultValue = "10") short strength,
-                         @RequestParam(value = "dexterity", defaultValue = "10") short dexterity,
-                         @RequestParam(value = "constitution", defaultValue = "10") short constitution,
-                         @RequestParam(value = "intelligence", defaultValue = "10") short intelligence,
-                         @RequestParam(value = "wisdom", defaultValue = "10") short wisdom,
-                         @RequestParam(value = "charisma", defaultValue = "10") short charisma) {
-        Long cnt = counter.incrementAndGet();
-        Character character = new Character(cnt, name, race, subrace,
-                gameClass, strength, dexterity, constitution, intelligence, wisdom, charisma);
-        characters.add(character);
+    private final Characters characters;
+
+    public CharacterController(Characters characters) {
+        this.characters = characters;
+    }
+
+    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
+    public Character create(@RequestBody Character character) {
+        characters.addCharacter(character);
         return character;
     }
 
-    @GetMapping()
+    @RequestMapping(produces = "application/json")
     public List<Character> list() {
-        return characters;
+        return characters.getCharacterList();
     }
 
-    @GetMapping("/{id}")
-    public Character getOne(@PathVariable Long id) {
-        return characters.stream()
-                .filter(p -> p.getId()==id)
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public Character getOneById(@PathVariable Long id) {
+        return characters.getOneById(id);
     }
-
 }
--- src/main/java/me/alexksysx/model/Character.java
+++ src/main/java/me/alexksysx/model/Character.java
@@ -1,32 +1,32 @@
 package me.alexksysx.model;
 
 public class Character {
-    private long id;
+    private Long id;
     private String name;
     private String race;
-    private String subrace;
-    private String gameClass;
-    private short strength, dexterity, constitution, intelligence, wisdom, charisma;
 
-    public Character(long id, String name, String race, String subrace, String gameClass, short strength, short dexterity, short constitution, short intelligence, short wisdom, short charisma) {
+    private Character() {}
+
+    public Character(Long id, String name, String race) {
         this.id = id;
         this.name = name;
         this.race = race;
-        this.subrace = subrace;
-        this.gameClass = gameClass;
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.constitution = constitution;
-        this.intelligence = intelligence;
-        this.wisdom = wisdom;
-        this.charisma = charisma;
     }
 
-    public long getId() {
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", race='" + race + '\'' +
+                '}';
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -44,69 +44,5 @@ public class Character {
 
     public void setRace(String race) {
         this.race = race;
-    }
-
-    public String getSubrace() {
-        return subrace;
-    }
-
-    public void setSubrace(String subrace) {
-        this.subrace = subrace;
-    }
-
-    public String getGameClass() {
-        return gameClass;
-    }
-
-    public void setGameClass(String gameClass) {
-        this.gameClass = gameClass;
-    }
-
-    public short getStrength() {
-        return strength;
-    }
-
-    public void setStrength(short strength) {
-        this.strength = strength;
-    }
-
-    public short getDexterity() {
-        return dexterity;
-    }
-
-    public void setDexterity(short dexterity) {
-        this.dexterity = dexterity;
-    }
-
-    public short getConstitution() {
-        return constitution;
-    }
-
-    public void setConstitution(short constitution) {
-        this.constitution = constitution;
-    }
-
-    public short getIntelligence() {
-        return intelligence;
-    }
-
-    public void setIntelligence(short intelligence) {
-        this.intelligence = intelligence;
-    }
-
-    public short getWisdom() {
-        return wisdom;
-    }
-
-    public void setWisdom(short wisdom) {
-        this.wisdom = wisdom;
-    }
-
-    public short getCharisma() {
-        return charisma;
-    }
-
-    public void setCharisma(short charisma) {
-        this.charisma = charisma;
     }
 }
