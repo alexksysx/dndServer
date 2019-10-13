3d7df280c6fa95619165f504f61c95b1cf3b310b
alexksysx <leha.bar.ru@gmail.com>
2019-10-13 21:19:11 +0300

    inheritance test1

--- src/main/java/me/alexksysx/model/CommonClass.java
+++ src/main/java/me/alexksysx/model/CommonClass.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @Entity
 @Proxy(lazy = false)
-@Table(name = "GAMECLASS")
+@Table(name = "commonclass")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(columnDefinition = "TYPE", name = "TYPE")
 @AllArgsConstructor
@@ -31,7 +31,6 @@ public class CommonClass {
     @Getter @Setter private String SavingThrow1;
     @Getter @Setter private String SavingThrow2;
     @Getter @Setter private int hitDice;
-    @Getter @Setter private boolean spellCasting;
     @Getter @Setter private boolean knowAllSpells;
 
 
