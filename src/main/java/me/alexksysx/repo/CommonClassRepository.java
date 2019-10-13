fc397f5b8b5b5a8dd86f59330514adf2d3e88e2b
alexksysx <leha.bar.ru@gmail.com>
2019-10-13 21:29:16 +0300

    inheritance test1

--- src/main/java/me/alexksysx/repo/CommonClassRepository.java
+++ src/main/java/me/alexksysx/repo/CommonClassRepository.java
@@ -2,6 +2,8 @@ package me.alexksysx.repo;
 
 import me.alexksysx.model.CommonClass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface CommonClassRepository extends CommonClassDAO<CommonClass>, JpaRepository<CommonClass, Long> {
 }
--- src/main/java/me/alexksysx/repo/SpellCasterRepository.java
+++ src/main/java/me/alexksysx/repo/SpellCasterRepository.java
@@ -2,6 +2,8 @@ package me.alexksysx.repo;
 
 import me.alexksysx.model.gameClasses.SpellCaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface SpellCasterRepository extends CommonClassDAO<SpellCaster>, JpaRepository<SpellCaster, Long> {
 }
