22b9b3a3ab97c5408e2e2897f75ff7d83094e895
alexksysx <leha.bar.ru@gmail.com>
2019-10-14 13:58:45 +0300

    inheritance test3

--- src/main/java/me/alexksysx/controller/GameClassController.java
+++ src/main/java/me/alexksysx/controller/GameClassController.java
@@ -3,6 +3,7 @@ package me.alexksysx.controller;
 import me.alexksysx.model.CommonClass;
 import me.alexksysx.model.gameClasses.SpellCaster;
 import me.alexksysx.repo.CommonClassRepository;
+import me.alexksysx.repo.SpellCasterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ public class GameClassController {
     @Autowired
     CommonClassRepository commonClassRepository;
 
+    @Autowired
+    SpellCasterRepository spellCasterRepository;
+
     @PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
     public CommonClass create(@RequestBody CommonClass commonClass) {
         commonClassRepository.save(commonClass);
@@ -21,7 +25,7 @@ public class GameClassController {
 
     @PostMapping(value = "/create/spellcaster", produces = "application/json", consumes = "application/json")
     public SpellCaster createSpellCaster(@RequestBody SpellCaster spellCaster) {
-        commonClassRepository.save(spellCaster);
+        spellCasterRepository.save(spellCaster);
         return spellCaster;
     }
 }
