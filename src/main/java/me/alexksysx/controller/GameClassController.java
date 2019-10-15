37077e38313cbc6dcbcce9b0c2741ac0fd8699ee
alexksysx <leha.bar.ru@gmail.com>
2019-10-15 21:00:54 +0300

    inheritance test7 with test db

--- src/main/java/me/alexksysx/controller/GameClassController.java
+++ src/main/java/me/alexksysx/controller/GameClassController.java
@@ -2,34 +2,36 @@ package me.alexksysx.controller;
 
 import me.alexksysx.model.CommonClass;
 import me.alexksysx.model.gameClasses.SpellCaster;
+import me.alexksysx.repo.CommonClassDAO;
 import me.alexksysx.repo.CommonClassRepository;
 import me.alexksysx.repo.SpellCasterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/class")
 public class GameClassController {
+
     @Autowired
     CommonClassRepository commonClassRepository;
 
     @Autowired
     SpellCasterRepository spellCasterRepository;
 
-    @PostMapping(value = "/create",/* consumes = "application/json",*/ produces = "application/json")
-    public CommonClass create(/*@RequestBody CommonClass commonClass*/) {
-        CommonClass commonClass = new CommonClass();
-        commonClass.setName("Test");
-        commonClass.setId(1L);
-        commonClass.setSource("Test");
-        commonClass.setDescription("TEST");
-        commonClass.setHitDice(3);
-        commonClass.setKnowAllSpells(false);
-        commonClass.setMainCharacteristic("TEST");
-        commonClass.setSecondCharacteristic("TEST");
-        commonClass.setSavingThrow1("TEST");
-        commonClass.setSavingThrow2("TEST");
+    @GetMapping(produces = "application/json")
+    public List<CommonClass> getAll() {
+        List<CommonClass> list = new ArrayList<>();
+        list.addAll(spellCasterRepository.findAll());
+        list.addAll(commonClassRepository.findAll());
+        return list;
+    }
+
+    @PostMapping(value = "/create", consumes = "application/json", produces = "application/json")
+    public CommonClass create(@RequestBody CommonClass commonClass) {
         commonClassRepository.save(commonClass);
         return commonClass;
     }
