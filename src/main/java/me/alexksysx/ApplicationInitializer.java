5f01591c206eaaf1d211a0288e63ff6c50b1f301
alexksysx <leha.bar.ru@gmail.com>
2020-02-05 21:08:29 +0300

    Test Spring Boot application

--- src/main/java/me/alexksysx/ApplicationInitializer.java
+++ src/main/java/me/alexksysx/ApplicationInitializer.java
@@ -1,27 +1,38 @@
 package me.alexksysx;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[]{
+//                ApplicationConfiguration.class
+//        };
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{
+//                "/*"
+//        };
+//    }
+//
+//
+//}
 
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{
-                ApplicationConfiguration.class
-        };
+@SpringBootApplication
+public class ApplicationInitializer extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInitializer.class, args);
     }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return null;
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{
-                "/*"
-        };
-    }
-
-
 }
