4cecf6e35ae58ca3e2b1d0ea06ca6ff6cda06f88
alexksysx <leha.bar.ru@gmail.com>
2020-02-05 21:24:55 +0300

    application.properties config for h2

--- src/main/java/me/alexksysx/ApplicationInitializer.java
+++ src/main/java/me/alexksysx/ApplicationInitializer.java
@@ -4,32 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-//public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-//
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return new Class[]{
-//                ApplicationConfiguration.class
-//        };
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return null;
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[]{
-//                "/*"
-//        };
-//    }
-//
-//
-//}
-
 @SpringBootApplication
 public class ApplicationInitializer extends SpringBootServletInitializer {
     public static void main(String[] args) {
